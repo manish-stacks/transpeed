203092043d1161a7f94760a08aa93fa5af640374
anish-stack <anishjha896@gmail.com>
2025-03-28 10:57:38 +0530

    Update next.config.mjs to add a comment for clarity

--- next.config.mjs
+++ next.config.mjs
@@ -4,5 +4,6 @@ const nextConfig = {
         domains: ['images.unsplash.com','assets.aceternity.com','flagcdn.com','i.ibb.co'], // Add the allowed image domain here
     },
 };
+// I am Don Your a re gone
 
 export default nextConfig;
