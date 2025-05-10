7d053e3ed7d7fb18138b253f85012b1f0dfd4e3c
vinaypandey1991 <vinaykp1991@gmail.com>
2025-05-10 18:12:53 +0530

    aircraft spacifications page

--- src/app/utility/aircarft-specification/page.jsx
+++ src/app/utility/aircarft-specification/page.jsx
@@ -1,32 +1,32 @@
-import { container } from '@/lib/container';
+'use client';
 import Image from 'next/image';
 import React from 'react';
 
 const Page = () => {
     return (
-        <div className='max-w-7xl mx-auto min-h-screen px-4 py-10 mt-20 mb-32'> 
-            <h1 className='text-3xl md:text-4xl font-semibold text-center text-slate-900 mb-8'>
+        <div className='max-w-7xl mx-auto min-h-screen px-4 py-10 mt-20 mb-32'>
+            {/* <h1 className='text-3xl md:text-4xl font-semibold text-center text-slate-900 mb-8'>
                 Air Craft Specification
-            </h1>
-            
-            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-                {container.conatiners.map((item, index) => (
-                    <div 
-                        key={index} 
-                        className='bg-white border border-black p-5 rounded-lg cursor-pointer flex flex-col items-center text-center hover:shadow-lg transition-shadow'
-                    >
-                        <Image 
-                            src={item.svg} 
-                            alt={item.name} 
-                            width={180} 
-                            height={180} 
-                            className='object-contain'
-                        />
-                        <h2 className='text-lg font-medium text-gray-700 mt-4'>
-                            {item.name}
-                        </h2>
-                    </div>
-                ))}
+            </h1> */}
+
+            <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6'>
+                <div className=''>
+                    <Image width={1920} height={1000} src="/images/air-craft-1.jpg" alt='Air craft 1' className='rounded border border-yellow-300 p-3 mb-5' />
+                    <Image width={1920} height={1000} src="/images/air-craft-2.webp" alt='Air craft 2' className='rounded border border-yellow-300 p-3 mb-5' />
+                    <Image width={1920} height={1000} src="/images/air-craft-3.webp" alt='Air craft 3' className='rounded border border-yellow-300 p-3 mb-5' />
+                    <Image width={1920} height={1000} src="/images/air-craft-4.webp" alt='Air craft 4' className='rounded border border-yellow-300 p-3 mb-5' />
+                    <Image width={1920} height={1000} src="/images/air-craft-5.webp" alt='Air craft 5' className='rounded border border-yellow-300 p-3 mb-5' />
+                    <Image width={1920} height={1000} src="/images/air-craft-6.webp" alt='Air craft 6' className='rounded border border-yellow-300 p-3 mb-5' />
+                    <Image width={1920} height={1000} src="/images/air-craft-7.webp" alt='Air craft 7' className='rounded border border-yellow-300 p-3 mb-5' />
+                    <Image width={1920} height={1000} src="/images/air-craft-8.webp" alt='Air craft 8' className='rounded border border-yellow-300 p-3 mb-5' />
+                    <Image width={1920} height={1000} src="/images/air-craft-9.webp" alt='Air craft 9' className='rounded border border-yellow-300 p-3 mb-5' />
+                    <Image width={1920} height={1000} src="/images/air-craft-10.webp" alt='Air craft 10' className='rounded border border-yellow-300 p-3 mb-5' />
+                    <Image width={1920} height={1000} src="/images/air-craft-11.webp" alt='Air craft 11' className='rounded border border-yellow-300 p-3 mb-5' />
+                    <Image width={1920} height={1000} src="/images/air-craft-12.webp" alt='Air craft 12' className='rounded border border-yellow-300 p-3 mb-5' />
+                    <Image width={1920} height={1000} src="/images/air-craft-13.webp" alt='Air craft 13' className='rounded border border-yellow-300 p-3 mb-5' />
+                    <Image width={1920} height={1000} src="/images/air-craft-14.webp" alt='Air craft 13' className='rounded border border-yellow-300 p-3 mb-5' />
+
+                </div>
             </div>
         </div>
     );
